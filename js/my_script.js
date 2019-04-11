78b6eacb4355d3816fd0a04e0dbc141b8aad7159
Oliveira <eliaspradoprofessional@outlook.com>
2019-04-11 00:10:55 +0000

    initial commit

---
+++ js/my_script.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function(){
+    jQuery( "tr:odd" ).css( "background-color", "#ffa366" );
+     jQuery( "tr:even" ).css( "background-color", "#ffd1b3" );
+     $("table").css("border", "solid 2px #cc5200");
+     $("table").css("position","center");
+});
